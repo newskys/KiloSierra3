c3e191b752aa0cc34339019ff0ad522a8be09450
Kyusik Shin <newskys1@gmail.com>
2020-11-08 22:15:47 +0900

    initial commit.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import {Grid, Typography} from "@material-ui/core";
+import Link from "@material-ui/core/Link";
 
 
 const Main: React.FC = () => {
   return (
     <>
-      test
+        <Container maxWidth="xs">
+            <TextField fullWidth label="Outlined" variant="outlined" margin="normal" />
+            <TextField fullWidth label="Outlined" variant="outlined" margin="normal" />
+
+            <Button fullWidth variant="contained" color="primary" style={{margin: '24px 0px 16px 0px'}}>
+                SIGN IN
+            </Button>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <Grid item xs={6}>
+                    <Typography variant="body2" color="primary">
+                        <Link href="#" onClick={() => {}} underline="hover">
+                            Forgot Password?
+                        </Link>
+                    </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="body2" color="primary">
+                        <Link href="#" onClick={() => {}} underline="hover">
+                            Don't have an account? Sign Up
+                        </Link>
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Container>
     </>
   )
 }

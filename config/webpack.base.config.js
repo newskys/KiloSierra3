ab75c61f099b9144767381f86d1dd57e8d55dc4f
siyoon-jung <siyoon.syj@gmail.com>
2021-01-17 23:12:17 +0900

    [study] config > add postcss-loader

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -28,6 +28,7 @@ module.exports = {
         use: [
           "style-loader", // Creates `style` nodes from JS strings
           "css-loader",   // Translates CSS into CommonJS
+          "postcss-loader",
           {
             loader: "sass-loader",   // Compiles Sass to CSS
             options: {

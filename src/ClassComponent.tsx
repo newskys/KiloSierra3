3b3f057b76c76e6ad702d0d3c59bff7b2155d036
siyoon-jung <siyoon.syj@gmail.com>
2021-01-23 13:45:49 +0900

    [study] setting > add styled-components

--- src/ClassComponent.tsx
+++ src/ClassComponent.tsx
@@ -1,4 +1,17 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+/* Adapt the colors based on primary prop */
+background: ${props => props.primary ? "palevioletred" : "white"};
+color: ${props => props.primary ? "white" : "palevioletred"};
+
+font-size: 1em;
+margin: 1em;
+padding: 0.25em 1em;
+border: 2px solid palevioletred;
+border-radius: 3px;
+`;
 
 class ClassComponent extends React.Component {
     // constructor(props) {
@@ -29,7 +42,14 @@ class ClassComponent extends React.Component {
 
     render() {
         return (
-            <>ClassComponent!</>
+            <>
+            
+            ClassComponent!
+
+            <Button>Normal</Button>
+            <Button primary>Primary</Button>
+            
+            </>
         )
     }
 }

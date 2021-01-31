b8cadb750b600569b82f3fea0086ef6147b1488e
Kyusik Shin <newskys@naver.com>
2021-01-31 22:23:31 +0900

    apply changes

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import { Box, Button, Card, CardMedia, CardContent, TextField, Typography } from '@material-ui/core';
+import { Avatar, Box, Button, Card, CardMedia, CardContent, Grid, TextField, Typography } from '@material-ui/core';
 import LockIcon from '@material-ui/icons/Lock';
 import Layout from '@components/ui/Layout';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
+    padding: '16px',
+  },
+
+  avatar_wrap: {
+    color: '#FF1493',
+    padding: '16px 0 0 0',
+  },
+
+  avatar: {
+    backgroundColor: '#FF1493',
+  },
+
+  login_input: {
+    margin: '16px 0 0 0',
+  },
+
+  signin: {
+    margin: '16px 0 0 0',
+    padding: '8px',
+  },
+
+  text_link: {
+    margin: '8px 0 0 0',
   },
 });
 
@@ -33,15 +56,24 @@ const LoginPage: React.FC = () => {
           </CardContent>
         </Card>
 
-        <LockIcon />
-
-        <form>
-          <TextField id="standard-basic" label="Standard" />
-          <TextField id="filled-basic" label="Filled" variant="filled" />
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-          <Button variant="contained" color="primary">SIGN IN</Button>
-          <Button href="#text-buttons" color="primary">Link</Button>
-        </form>
+        <Box className={classes.avatar_wrap}>
+          {/* <Avatar className={classes.avatar}> */}
+            <LockIcon />
+          {/* </Avatar> */}
+        </Box>
+        <TextField fullWidth className={classes.login_input} variant="outlined" label="Email Address *" margin="normal" />
+        <TextField fullWidth className={classes.login_input} variant="outlined" label="Password *" margin="normal" />
+        <Button className={classes.signin} fullWidth variant="contained" color="primary">SIGN IN</Button>
+        <Grid className={classes.text_link} container>
+          <Grid item xs={6}>
+            <Button href="#text-buttons" color="primary">Forgot Password?</Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button href="#text-buttons" color="primary">Don't have an account? Sign Up</Button>
+          </Grid>
+        </Grid>
+        
+      
       </Box>
     </Layout>
   )

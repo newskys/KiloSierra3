bbb1466a5cb2dd21f3b295823fde982fcf8b12ae
Kyusik Shin <>
2021-02-19 22:27:41 +0900

    add scheduler lib

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -3,11 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import Layout from '@components/ui/Layout';
 import LoginPage from '@components/login/LoginPage';
 import { HOME, LOGIN, MAIN } from '@common/routePath';
+import HomePage from '@components/login/HomePage';
 
 const Router: React.FC = () => {
     return (
         <Switch>
-            <Route exact path={[MAIN, HOME]} component={Layout} />
+            <Route exact path={[MAIN, HOME]} component={HomePage} />
             <Route exact path={LOGIN} component={LoginPage} />
             {/* <Layout /> */}
         </Switch>

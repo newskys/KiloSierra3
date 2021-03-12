26d269ea23461f0ef871bd721420b25652fb9ec8
Kyusik Shin <kyusik.shin@navercorp.com>
2021-03-12 23:19:52 +0900

    add icons and functions

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,14 +1,24 @@
 import {
   AppBar,
   Button,
+  Divider,
+  Drawer,
   IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Toolbar,
   Typography,
 } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 
 const useStyles = makeStyles({
   root: {
@@ -29,6 +39,9 @@ const useStyles = makeStyles({
     flex: '0 0 auto',
     height: '66px',
   },
+  list: {
+    width: 250,
+  },
 })
 
 interface Props {
@@ -45,11 +58,39 @@ const Header: React.FC<Props> = ({
   onClickLogout,
 }) => {
   const classes = useStyles()
-
+  const [isDrawerOpened, setDrawerOpened] = useState<boolean>(false)
+  const list = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={(e) => setDrawerOpened(false)}
+      onKeyDown={(e) => setDrawerOpened(false)}>
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <AccountCircleIcon />
+          </ListItemIcon>
+          <ListItemText primary={'My Account'} />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button onClick={(e) => onClickLogout(e)}>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText primary={'Logout'} />
+        </ListItem>
+      </List>
+    </div>
+  )
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="lecturer's profile">
           <AccountCircleIcon onClick={(e) => onClickHamburger(e)} />
         </IconButton>
         <Typography component="h1" variant="h6" className={classes.title}>
@@ -57,12 +98,24 @@ const Header: React.FC<Props> = ({
         </Typography>
 
         {isLogin ? (
-          <MenuIcon />
+          <>
+            <IconButton color="inherit" aria-label="my schedule">
+              <EventNoteIcon />
+            </IconButton>
+            <IconButton edge="end" color="inherit" aria-label="menu">
+              <MenuIcon onClick={(e) => setDrawerOpened(true)} />
+            </IconButton>
+            <Drawer
+              anchor={'right'}
+              open={isDrawerOpened}
+              onClose={(e) => setDrawerOpened(false)}>
+              {list()}
+            </Drawer>
+          </>
         ) : (
-          <Button
-            color="inherit"
-            onClick={(e) => onClickLogin(e)
-            }>Login</Button>
+          <Button color="inherit" onClick={(e) => onClickLogin(e)}>
+            Login
+          </Button>
         )}
       </Toolbar>
     </AppBar>
